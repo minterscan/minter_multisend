8a6497c55f328988ab6058a9f8257b4c55c75c88
whitesunset <chester.work@gmail.com>
2019-06-26 01:40:41 +0200

    Fix baseUrl

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo">
-    <img alt="Minterscan" src="/img/minter.png" width="100">
+    <img alt="Minterscan" src="img/minter.png" width="100">
     <div class="title is-5 has-text-centered">Minter Multisend</div>
   </div>
 </template>
