7d6dfac1d1eb1934f55654b704890d97c5175b69
whitesunset <chester.work@gmail.com>
2019-06-26 01:41:11 +0200

    Use synced mainnet URL

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
   explorerBaseUrl: 'https://minterscan.net',
-  mainnetUrl: 'https://minter-node-1.testnet.minter.network',
+  mainnetUrl: 'https://minterscan.pro/explorer',
   explorerApiBaseUrl: 'https://explorer-api.minter.network/api/v1'
 }
