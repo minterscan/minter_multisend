f6e527d25da1e4f8e25554eda741f83144d430dd
whitesunset <chester.work@gmail.com>
2020-07-10 15:34:10 +0200

    Update mainnet URL

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   addressesLimit: 100,
   explorerBaseUrl: 'https://minterscan.net',
-  mainnetUrl: 'https://node-main.minter.su',
+  mainnetUrl: 'https://api.minter.one',
   explorerApiBaseUrl: 'https://explorer-api.minter.network/api/v1'
 }
